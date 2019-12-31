a771827ea9b0ec2f036c0832414d61948ebecdd6
陈昱安 <31612534@qq.com>
2019-12-31 11:39:05 +0800

    demoTest
    Signed-off-by: 陈昱安 <31612534@qq.com>

--- chenlaoshi/main.cpp
+++ chenlaoshi/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
+void jiafa(int a, int b);
 int main()
 {
     cout << "hello world" << endl;
+}
+void jiafa(int a, int b)
+{
+    std::cout << a + b << std::endl;
 }
