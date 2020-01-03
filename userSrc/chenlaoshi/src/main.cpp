6a4b03e526f44ea8eb950d793aea2e9cb56c057c
陈昱安 <31612534@qq.com>
2020-01-03 12:12:44 +0800

    update
    Signed-off-by: 陈昱安 <31612534@qq.com>

---
+++ userSrc/chenlaoshi/src/main.cpp
@@ -0,0 +1,13 @@
+#include "../include/main.hpp"
+extern "C" {
+#include "SDL2/SDL.h"
+};
+
+int main(int argc, char *argv[]) {
+  if (SDL_Init(SDL_INIT_VIDEO)) {
+    printf("Could not initialize SDL - %s\n", SDL_GetError());
+    return -1;
+  }
+  printf("init SDL success!\n");
+  return 0;
+}
