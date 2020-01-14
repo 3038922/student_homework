655d4a2d55182882a45a0b28fd2cbaf30fff222c
bobo2009125 <2668064495@qq.com>
2020-01-14 14:36:50 +0800

    bobo写了游戏角色
    Signed-off-by: bobo2009125 <2668064495@qq.com>

--- userSrc/bobo/src/main.cpp
+++ userSrc/bobo/src/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+using namespace std;
 int main() {
-  std::cout << "\nwo jiao bobo\n" << std::endl;
+  system("chcp 65001");
+  cout << "姓名:百里守约\n性别:男" << endl;
+  cout << "种族:人" << endl;
+  cout << "职业:射手 " << endl;
+  cout << "技能:1.狂风之息\n2.静谧之眼\n"
+          "3.不知道叫什么，反正往指定的方向射一枪并往后跳"
+       << endl;
+  cout << "攻击力:600\n防御力:5000" << endl;
+  cout << "血量:3000\n攻击范围:20米" << endl;
   return 0;
 }
