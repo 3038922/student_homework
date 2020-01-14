58ed3fb6f54f708d334805649b882062a8eb0780
cow <898934967qq.com>
2020-01-14 16:54:08 +0800

    cow写的游戏
    Signed-off-by: cow <898934967qq.com>

--- userSrc/cow/src/main.cpp
+++ userSrc/cow/src/main.cpp
@@ -2,11 +2,18 @@
 using namespace std;
 int main() {
   system("chcp 65001");
-  std::cout << "姓名：凯\t性别：男\n" << endl;
-  cout << "种族：凯——坦克\n" << endl;
-  cout << "波波：1.无敌回旋——攻击力100000\t2.万米暴击——攻击力100000\t3."
-          "五连绝杀——攻击力10000\t"
-       << endl;
-  cout << "生命力：10000000" << endl;
+  std::string asd;
+  int aj;
+  cout << "请输入您的账号:";
+  cin >> asd;
+  cout << "您的等级是: ";
+  cin >> aj;
+  std::cout << "姓名：凯皇\n性别：男♂\n";
+  cout << "种族：凯皇——战士\n";
+  cout << "技能1攻击力: " << 1000000 * aj << "\n";
+  cout << "技能2攻击力: " << 1000000 * aj << "\n";
+  cout << "技能3攻击力: " << 1000008 * aj << "\n";
+  cout << "生命力：" << 10000 * aj << "\n" << endl;
+
   return 0;
 }
