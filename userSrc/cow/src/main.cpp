13a835605acc65b626103ccb7128cc37caacc2bc
cow <898934967qq.com>
2020-01-16 15:29:31 +0800

    cow的作业
    Signed-off-by: cow <898934967qq.com>

--- userSrc/cow/src/main.cpp
+++ userSrc/cow/src/main.cpp
@@ -3,12 +3,27 @@ using namespace std;
 int main()
 {
     system("chcp 65001");
-    std::string asd;
-    int aj;
-    cout << "请输入您的账号:";
-    cin >> asd;
-    cout << "您的等级是: ";
-    cin >> aj;
+    string z = "liuluozheng";
+    string m = "8982E";
+    string z1;
+    string m1;
+    int aj = 1;
+    cout << "请输入您的账号" << endl;
+    cin >> z1;
+    cout << "请输入您的密码" << endl;
+    cin >> m1;
+    if (z != z)
+    {
+
+        return 0;
+    }
+    if (m != m1)
+    {
+
+        cout << "你输入的账号和密码错误!!!" << endl;
+        return 0;
+    }
+    cout << "您的等级是: " << aj << std::endl;
     std::cout << "姓名：凯皇\n性别：男♂\n";
     cout << "种族：凯皇——战士\n";
     cout << "技能1攻击力: " << 1000000 * aj << "\n";
