16c0a410307ceb3d6e7fb644080c46e29a6a6261
bobo2009125 <2668064495@qq.com>
2020-01-18 13:55:55 +0800

    bobo改了作业
    Signed-off-by: bobo2009125 <2668064495@qq.com>

--- userSrc/bobo/src/main.cpp
+++ userSrc/bobo/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 int main()
 {
 
@@ -8,51 +9,16 @@ int main()
     std::string mima = "bb2009125";
     std::string account1;
     std::string mima1;
-    int level = 55;
-    std::cout << "请输入您的游戏账号:";
-    std::cin >> account1;
-    std::cout << "请输入您的游戏密码:";
-    std::cin >> mima1;
+    int level = 1;
+    int x = 5;
+    int y = 6;
+    int z = 11;
+    std::cout << ((x == y) && (x == z) && (y == z)) << std::endl;
+
+    std::cout << ((x != y) || (x == z) || (y != z)) << std::endl;
+    // std::cout << "请输入您的游戏账号:";
+    // std::cin >> account1;
+    // std::cout << "请输入您的游戏密码:";
+    // std::cin >> mima1;
     // std::cout << "*****************您的游戏账号名字是:" << account << std::endl;
-    if (account1 != account)
-    {
-        std::cout << "你输入的账号错误" << std::endl;
-        return 0;
-    }
-
-    if (mima1 != mima)
-    {
-        std::cout << "你输入的密码错误" << std::endl;
-        return 0;
-    }
-    std::cout << "您的等级是:" << level << std::endl;
-    std::cout << "*********************姓名:百里守约\n性别:男*************************" << std::endl;
-    std::cout << "种族:人*************************" << std::endl;
-    std::cout << "*************************职业:射手 " << std::endl;
-    std::cout << "技能:1.狂风之息2.静谧之眼3.逃脱" << std::endl;
-    std::cout << "*************************攻击力:" << 600 * level << std::endl;
-    std::cout << "防御力:" << 50 * level;
-    std::cout << "攻击范围:" << 10 * level * 0.5;
-
-    std::cout << "血量:" << 5000 * level * 0.5;
-    // system("chcp 65001");
-    // system("cls");
-    // int x = 125;
-    // int y;
-    // std::cout << "**********猜数字游戏**********" << std::endl;
-    // std::cout << "*********请输入一个整数*********" << std::endl;
-    // std::cin >> y;
-    // if (y > x)
-    // {
-    //     std::cout << "你猜大了,被宇宙中心的黑洞吞噬了!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
-    // }
-    // else if (y == x)
-    // {
-    //     std::cout << "恭喜你你猜对了,赏你一个你最爱吃的黄金大嘴巴子,你可以去买彩票了!" << std::endl;
-    // }
-    // else
-    // {
-    //     std::cout << "你猜小了,被脱光了衣服扔到基因改造牢里改造成傻逼了!!!!!!!!!!!!!" << std::endl;
-    // }
-    return 0;
 }
