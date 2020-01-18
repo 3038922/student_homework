5e0934195d99e3d5b0b8292e44152b6be8e5e0b9
bobo2009125 <2668064495@qq.com>
2020-01-18 15:15:34 +0800

    bobo交了作业
    Signed-off-by: bobo2009125 <2668064495@qq.com>

--- userSrc/bobo/src/main.cpp
+++ userSrc/bobo/src/main.cpp
@@ -10,31 +10,41 @@ int main()
     std::string account1;
     std::string mima1;
     int level = 1;
+    int x = 1;
+    while (x > 0)
+    {
+        std::cout << "阿斯蒂芬\t"
+                  << "第" << x << "次" << std::endl;
+        x++;
+    }
     // int x = 5;
     // int y = 6;
     // int z = 11;
-    std::cout << "请输入您的游戏账号:";
-    std::cin >> account1;
-    std::cout << "请输入您的游戏密码:";
-    std::cin >> mima1;
-    // std::cout << "*****************您的游戏账号名字是:" << account << std::endl;
-    // std::cout << ((x == y) && (x == z) && (y == z)) << std::endl;
+    // std::cout << "请输入您的游戏账号:";
+    // std::cin >> account1;
+    // std::cout << "请输入您的游戏密码:";
+    // std::cin >> mima1;
+    // // std::cout << "*****************您的游戏账号名字是:" << account << std::endl;
+    // // std::cout << ((x == y) && (x == z) && (y == z)) << std::endl;
 
-    // std::cout << ((x != y) || (x == z) || (y != z)) << std::endl;
-    if (account1 == account && (mima1 == mima))
-    {
-        std::cout << "您的等级是:" << level << std::endl;
-        std::cout << "*********************姓名:百里守约\n性别:男*************************" << std::endl;
-        std::cout << "种族:人*************************" << std::endl;
-        std::cout << "*************************职业:射手 " << std::endl;
-        std::cout << "技能:1.狂风之息2.静谧之眼3.逃脱" << std::endl;
-        std::cout << "*************************攻击力:" << 600 * level << std::endl;
-        std::cout << "防御力:" << 50 * level;
-        std::cout << "攻击范围:" << 10 * level * 0.5;
+    // // std::cout << ((x != y) || (x == z) || (y != z)) << std::endl;
+    // if (account1 == account && (mima1 == mima))
+    // {
+    //     std::cout << "您的等级是:" << level << std::endl;
+    //     std::cout << "*********************姓名:百里守约\n性别:男*************************" << std::endl;
+    //     std::cout << "种族:人*************************" << std::endl;
+    //     std::cout << "*************************职业:射手 " << std::endl;
+    //     std::cout << "技能:1.狂风之息2.静谧之眼3.逃脱" << std::endl;
+    //     std::cout << "*************************攻击力:" << 600 * level << std::endl;
+    //     std::cout << "防御力:" << 50 * level;
+    //     std::cout << "攻击范围:" << 10 * level * 0.5;
 
-        std::cout << "血量:" << 5000 * level * 0.5;
-    }
-    std::cout << "您输入的账号或密码错误" << std::endl;
+    //     std::cout << "血量:" << 5000 * level * 0.5;
+    // }
+    // else
+    // {
+    //     std::cout << "您输入的账号或密码错误" << std::endl;
+    // }
     // #include <iostream>
     // int main()
     // {
