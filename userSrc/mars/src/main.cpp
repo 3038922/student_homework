93b9365eaaae8aa623309cb1269df97c6e32810d
mars <1834527271@qq.com>
2020-01-18 13:12:29 +0800

    mars更改的程序
    Signed-off-by: mars <1834527271@qq.com>

--- userSrc/mars/src/main.cpp
+++ userSrc/mars/src/main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
+using namespace std;
 int main()
 {
-
     system("cls");
     system("chcp 65001");
     std::string account;
-    int level = 0;
+    std::string zhanghao = "lucas";
+    std::string mima = "20110312";
+    std::string zhanghao1;
+    std::string mima1;
+    int level = 1;
     std::cout << "请输入您的账号: ";
     std::cin >> account;
     std::cout << "请输入您的等级: ";
     std::cin >> level;
     std::cout << "您的账号：" << account;
-    std::cout << "\n您的姓名：白起";
+    if (zhanghao1 != zhanghao1)
+    {
+        std::cout << "账号错了 " << std::endl;
+        return 0;
+    }
+    if (mima != mima1)
+    {
+        std::cout << "密码错了" << std::endl;
+        return 0;
+    }
+    std::cout << "\n姓名：白起";
     std::cout << "\n蓝条：" << 80 * level * 19;
     std::cout << "\n防御力：" << 98880 * level * 90;
     std::cout << "\n攻击力：" << 90000 * level * 10;
     std::cout << "\n生命：" << 100000 * level * 0.2;
     std::cout << "\n等级：10" << level;
-
     return 0;
 }
