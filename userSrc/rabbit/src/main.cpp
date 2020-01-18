f180d5f609ad56bea40c0f54d06ac758afb15adc
lixuanyu <1572129832@qq.com>
2020-01-18 17:06:29 +0800

    rabbit 更改了程序
    Signed-off-by: lixuanyu <1572129832@qq.com>

--- userSrc/rabbit/src/main.cpp
+++ userSrc/rabbit/src/main.cpp
@@ -1,29 +1,38 @@
 #include <iostream>
 int main()
 {
-
     system("chcp 65001");
     system("cls");
-    std::string zhanghao = "adsf";
+    std::string zhanghao = "asdf";
     std::string mima = "20110215";
     std::string zhanghao1;
     std::string mima1;
     int level = 1;
-    std::cout << "请输入您的账号: ";
-    std::cin >> zhanghao1;
-    std::cout << "请输入您的密码: ";
-    std::cin >> mima1;
-    if (zhanghao1 != zhanghao)
+
+    int jishuqi = 0;
+    while (jishuqi < 3)
     {
-        std::cout << "你输入的账号错误" << std::endl;
-        return 0;
+        std::cout << "请输入您的账号: ";
+        std::cin >> zhanghao1;
+        std::cout << "请输入您的密码: ";
+        std::cin >> mima1;
+        if ((zhanghao1 == zhanghao) && (mima1 == mima))
+        {
+            std::cout << "输入正确,登录成功" << std::endl;
+            break; //跳出当前循环
+        }
+        else
+        {
+            std::cout << "输入错误,请再试一次" << std::endl;
+        }
+        jishuqi++;
+        if (jishuqi >= 3)
+        {
+            std::cout << "你是大笨蛋 输了 " << jishuqi << " 次还错!!!!" << std::endl;
+            return 0; //结束程序 后面的话不执行了.
+        }
     }
 
-    if (mima1 != mima)
-    {
-        std::cout << "你输入的密码错误" << std::endl;
-        return 0;
-    }
     std::cout << "等 级:";
     std::cin >> level;
     std::cout << "姓 名：关公\n"
@@ -37,6 +46,5 @@ int main()
     std::cout << "生 命：" << 100 * level * 1.5 << std::endl;
     std::cout << "攻 击 力：" << 199 * level * 4.0 << std::endl;
     std::cout << "防 御 力" << 50 * level * 0.5 << std::endl;
-
     return 0;
 }
