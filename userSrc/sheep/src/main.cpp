c9d35a0c43702e7151a85eb8ac1418e77ceb5e27
陈昱安 <31612534@qq.com>
2020-02-21 20:40:05 +0800

    sheep 新作业
    Signed-off-by: 陈昱安 <31612534@qq.com>

--- userSrc/sheep/src/main.cpp
+++ userSrc/sheep/src/main.cpp
@@ -9,6 +9,20 @@ std::string gongchang(std::string a)
     else
         return "我是牛肉厂 只加工牛肉";
 }
+void player(std::string name, int level, std::string job, std::string skill1, std::string skiil2, std::string skill3)
+{
+    std::cout << "等  级: " << dengji << "\n"
+              << "姓  名: 后羿\n"
+              << "性  别: 男♂\n"
+              << "职  业: 射手\n"
+              << "技能 1: 多重箭矢\n"
+              << "技能 2: 落日余晖\n"
+              << "大  招: 灼日之矢\n"
+              << "血  量: " << 250 * dengji << "\n"
+              << "攻击力: " << 50 * dengji << "\n"
+              << "防御力: " << 20 * dengji << "\n"
+              << std::endl;
+}
 int main()
 {
     std::cout << jiafa(1, 2) << std::endl;
