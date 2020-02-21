2a14344e0cd8e94d4abbb7c864a4091c97468c28
fish <2940028639@qq.com>
2020-02-21 20:42:14 +0800

    fish交了作业
    Signed-off-by: fish <2940028639@qq.com>

--- userSrc/fish/src/main.cpp
+++ userSrc/fish/src/main.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
+int jiafa(int a, int b)
+{
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << a + b << std::endl;
+    return a + b;
+}
+double xiaoshujiafa(int c, int d)
+{
+    std::cin >> c;
+    std::cin >> d;
+    std::cout << c + d << std::endl;
+    return c + d;
+}
+std::string zifu(std::string e)
+{
+    if (e == "鱼")
+    {
+        std::cout << "我是鱼" << std::endl;
+    }
+    else
+    {
+        std::cout << "鱼是我" << std::endl;
+    }
+    return e;
+}
 int main()
 {
     system("chcp 65001");
-    std::string a;
-    int b;
-    int c;
-    std::cout << "请输入召唤师名字:";
-    std::cin >> a;
-    std::cout << "您的召唤师的姓名是:" << a << std::endl;
-    std::cout << "请输入你的召唤师等级:";
-    std::cin >> b;
-    std::cout << "您的召唤师等级是:" << b << std::endl;
-    std::cout << "请选择你的英雄:(1.亚瑟...暂代开发)";
-    std::cin >> c;
-    std::cout << "技能1:圣剑冲锋(攻击力:1000)" << std::endl;
-    std::cout << "技能2:圣剑守护(攻击力:500)" << std::endl;
-    std::cout << "大招:圣剑审判(攻击力:3000)" << std::endl;
-    std::cout << "职业:战士" << std::endl;
-    std::cout << "生命:" << 5000 * b * 1.3 << std::endl;
-    std::cout << "蓝量:无" << std::endl;
-    std::cout << "攻击力:" << 300 * b * 0.7 << std::endl;
-    std::cout << "防御:" << 200 * b * 1 << std::endl;
+    std::cout << jiafa(1, 2) << std::endl;
+    std::cout << "xiaoshujiafa(1.1,1.2)" << std::endl;
+    std::cout << "zifu(鱼)" << std::endl;
 }
