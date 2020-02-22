fe40b006305bca160b12af8735085fc54b3b0a09
guaji <534869228@qq.com>
2020-02-22 21:47:21 +0800

    AFK提交作业
    Signed-off-by: guaji <534869228@qq.com>

--- userSrc/guaji/src/main.cpp
+++ userSrc/guaji/src/main.cpp
@@ -8,8 +8,23 @@ void player(std::string Name, std::string job, int Level, int life, int Attack, 
     std::cout << "攻击力:" << Attack * Level << std::endl;
     std::cout << "防御力:" << Defense * Level << std::endl;
 }
+void jh(int *a, int *b)
+{
+    if (*a < *b)
+    {
+        int ls = *b;
+        *b = *a;
+        *a = ls;
+    }
+}
 int main()
 {
-    player("吕布", "战士", 15, 1000, 200, 150);
+    system("chcp 65001");
+    int a = 100;
+    int b = 200;
+    std::cout << "交换前:a=" << a << "\tb=" << b << std::endl;
+    jh(&a, &b);
+    std::cout << "交换后:a=" << a << "\tb=" << b << std::endl;
+    //player("吕布", "战士", 15, 1000, 200, 150);
     return 0;
 }
