c13123d940c9a4b345aa40ce4ffd69e16aaa29c3
陈昱安 <31612534@qq.com>
2020-02-24 21:52:19 +0800

    修正好了
    Signed-off-by: 陈昱安 <31612534@qq.com>

--- userSrc/chenlaoshi/src/main.cpp
+++ userSrc/chenlaoshi/src/main.cpp
@@ -1,14 +1,32 @@
 #include "../include/userAPI.hpp"
-
+#include <iostream>
+void swap(int **a, int **b)
+{
+    int *temp = *a;
+    *a = *b;
+    *b = temp;
+}
 int main(int argc, char *argv[])
-{ /*Initialize LittlevGL*/
+{
+
+    int *a, *b;
+    a = new int(10);
+    b = new int(20);
+
+    std::cout << "交换前指向的地址 a:" << a << " b:" << b << std::endl;
+    std::cout << "交换前值 a:" << *a << " b:" << *b << std::endl;
+
+    swap(&a, &b);
+    std::cout << "交换后指向地址 a:" << a << " b:" << b << std::endl;
+    std::cout << "交换后值 a:" << *a << " b:" << *b << std::endl;
+    delete a;
+    delete b;
+    a = nullptr;
+    b = nullptr;
+
     lv_init();
-
-    /*Initialize the HAL for LittlevGL*/
     hal_init();
-
     demo_create();
-
     while (1)
     {
         lv_task_handler();
