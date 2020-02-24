61afc7719c947a1ba8085e57253bd9b1b8cdc99b
chicken <15067000960@163.com>
2020-02-24 19:16:37 +0800

    chicken上传作业
    Signed-off-by: chicken <15067000960@163.com>

--- userSrc/chicken/src/main.cpp
+++ userSrc/chicken/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+/*
 int zhengshu(int a)
 {
     return a;
@@ -12,11 +13,24 @@ std::string zifu(std::string a)
 {
     return (a == "chicken") ? "shadoulesschicken" : "chicken";
 }
+*/
+void waigua(int *a, int *b)
+{
+    int temp = *b;
+    *b = *a;
+    *a = temp;
+}
 int main()
 {
     system("chcp 65001");
-    cout << zhengshu(3) << endl;
-    cout << xiaoshu(3.5) << endl;
-    cout << zifu("chicken") << endl;
+    int a = 100, b = 200;
+    cout << "更改前:";
+    cout << "a = " << a << " b = " << b << endl;
+    waigua(&a, &b);
+    cout << "更改后:";
+    cout << "a = " << a << " b = " << b << endl;
+    //cout << zhengshu(3) << endl;
+    //cout << xiaoshu(3.5) << endl;
+    //cout << zifu("chicken") << endl;
     return 0;
 }
