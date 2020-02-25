aef42a17cba094aaa8bf0e5b75d57320b52392ab
glass <3544505221@qq.com>
2020-02-25 18:08:01 +0800

    glass交了作业
    Signed-off-by: glass <3544505221@qq.com>

--- userSrc/glass/src/main.cpp
+++ userSrc/glass/src/main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 #include <time.h>
+1 ref void swap(int **a, int **b)
+{
+    int *temp = *a;
+    *a = *b;
+    *b = temp;
+}
+1 ref int main(int argc, char *argv[])
+    H
+    int *a,
+    *b;
+a = new int(10);
+b = new int(20);
+std::cout << "交换前指向的地址 a:" << a << "b:" << b << std::endl;
+std::cout << "交换前值a:<<*a<" b : "<<b<<std::endl;
+                                   swap(6a, 6b);
+std::cout << "交换后指向地址a:" << a << "b:" << b << std::endl;
+std::cout << "交换后值a:" << *a << "b:" << *b << std::endl;
+delete a;
+delete b;
+a = nullptr;
+b = nullptr;
+You, a few seconds ago Uncommitted changes
+     lv_init();
+hal_init();
+demo_create();
+while (1)
+{
+    lv_task_handler();
+    Sleep(10); /*Just to let the system breathe*/
+}
+return0;
+
 int main()
 {
     std::system("chcp 65001");
@@ -86,33 +118,32 @@ int main()
         return 0;
     }
 }
-{
-    srand(time(NULL)); //生成随机数
-    std::string zhanghao = "qaz";
-    std::string mima = "123";
-    std::string zhanghao1;
-    std::string mima1;
-    int p1Hp = 550;           //设置玩家1血量
-    int p2Hp = 500;           //设置玩家2血量
-    int P1gong[2] = {20, 60}; //设置玩家一攻击力上下限
-    int P2gong[2] = {25, 65}; //设置玩家二攻击力上下限
+srand(time(NULL)); //生成随机数
+std::string zhanghao = "qaz";
+std::string mima = "123";
+std::string zhanghao1;
+std::string mima1;
+int p1Hp = 550;           //设置玩家1血量
+int p2Hp = 500;           //设置玩家2血量
+int P1gong[2] = {20, 60}; //设置玩家一攻击力上下限
+int P2gong[2] = {25, 65}; //设置玩家二攻击力上下限
 
-    while (1)
+while (1)
+{
+    int P1g = rand() % (P1gong[1] - P1gong[0]) + P1gong[0]; //计算玩家1攻击力
+    int P2g = rand() % (P2gong[1] - P2gong[0]) + P2gong[0]; //计算玩家2攻击力
+    p2Hp -= P1g;                                            //玩家二受到攻击
+    if (p2Hp <= 0)                                          //如果玩家2血量小于1，说明死了，跳出循环；
     {
-        int P1g = rand() % (P1gong[1] - P1gong[0]) + P1gong[0]; //计算玩家1攻击力
-        int P2g = rand() % (P2gong[1] - P2gong[0]) + P2gong[0]; //计算玩家2攻击力
-        p2Hp -= P1g;                                            //玩家二受到攻击
-        if (p2Hp <= 0)                                          //如果玩家2血量小于1，说明死了，跳出循环；
-        {
-            std::cout << "p1 win!" << std::endl;
-            break;
-        }
-        std::cout << "p2-" << P1g << "点血 还剩" << p2Hp << "点血" << std::endl; //输出每次
-        p1Hp -= P2g;                                                             //玩家1受到攻击
-        if (p1Hp <= 0)                                                           //如果玩家1血量小于1，说明死了，跳出循环；
-        {
-            std::cout << "p2 win!" << std::endl;
-            break;
-        }
-        std::cout << "p1-" << P2g << "点血 还剩" << p1Hp << "点血" << std::endl; //输出每次
+        std::cout << "p1 win!" << std::endl;
+        break;
     }
+    std::cout << "p2-" << P1g << "点血 还剩" << p2Hp << "点血" << std::endl; //输出每次
+    p1Hp -= P2g;                                                             //玩家1受到攻击
+    if (p1Hp <= 0)                                                           //如果玩家1血量小于1，说明死了，跳出循环；
+    {
+        std::cout << "p2 win!" << std::endl;
+        break;
+    }
+    std::cout << "p1-" << P2g << "点血 还剩" << p1Hp << "点血" << std::endl; //输出每次
+}
