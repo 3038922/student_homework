98b47ffcab1427f30b29b4d8bd8b63cd426ed8b7
guaji <534869228@qq.com>
2020-02-26 20:30:05 +0800

    AFK提交作业
    Signed-off-by: guaji <534869228@qq.com>

--- userSrc/guaji/src/main.cpp
+++ userSrc/guaji/src/main.cpp
@@ -1,4 +1,62 @@
 #include <iostream>
+#include <windows.h>
+void 菱形()
+{
+    //上半段
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8 - i; j++)
+        {
+            printf(" ");
+        }
+
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    //下半段
+    for (int i = 8 - 2; i >= 0; i--)
+    {
+        for (int j = 0; j < (8 - i); j++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+void 直角三角()
+{
+    for (int i = 1; i <= 8; i++)
+    {
+        for (int j = 1; j < i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+void 等腰三角()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8 - i; j++)
+        {
+            printf(" ");
+        }
+
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
 void player(std::string Name, std::string job, int Level, int life, int Attack, int Defense)
 {
     std::cout << "姓名:" << Name << std::endl;
@@ -14,13 +72,13 @@ void swap(std::string *a, std::string *b)
     *a = *b;
     *b = temp;
 }
-void swap1(std::string *a, std::string *b)
+void swap(std::string **a, std::string **b)
 {
-    std::string **temp = &a;
-    a = b;
-    b = *temp;
+    std::string *temp = *a;
+    *a = *b;
+    *b = temp;
 }
-void jh(int *a, int *b)
+void swap(int *a, int *b)
 {
     if (*a < *b)
     {
@@ -32,17 +90,36 @@ void jh(int *a, int *b)
 int main()
 {
     system("chcp 65001");
-    std::string class1 = "1234";
-    std::string class2 = "5678";
-    std::cout << "交换前:class1=" << class1 << "\tclass2=" << class2 << std::endl;
-    swap1(&class1, &class2);
-    std::cout << "交换后:class1=" << class1 << "\tclass2=" << class2 << std::endl;
-    //int a, b;
-    //std::cout << "请输入要交换的数字:" << std::endl;
-    //std::cin >> a >> b;
-    //std::cout << "交换前:a=" << a << "\tb=" << b << std::endl;
-    //jh(&a, &b);
-    //std::cout << "交换后:a=" << a << "\tb=" << b << std::endl;
-    //player("吕布", "战士", 15, 1000, 200, 150);
+    直角三角();
+    std::cout << "\n";
+    等腰三角();
+    std::cout << "\n";
+    菱形();
+    /*
+    std::string *a = new std::string("abcd");
+    std::string *b = new std::string("dcba");
+    std::cout << "交换前:a=" << *a << "\tb=" << *b << std::endl;
+    swap(&a, &b);
+    std::cout << "交换后:a=" << *a << "\tb=" << *b << std::endl;
+
+    std::cout << "指针a本身的地址大小:" << sizeof(&a) << " 指针a本身地址:" << &a << std::endl;
+    std::cout << "指针a指向的地址的大小:" << sizeof(a) << " 指针a指向的地址:" << a << std::endl;
+    std::cout << "指针a指向的地址的值大小:" << sizeof(*a) << " 指针a指向的值:" << *a << std::endl;
+    delete a;
+    delete b;
+    a = nullptr;
+    b = nullptr;
+    std::cout << "删除后:" << std::endl;
+    std::cout << "指针a本身的地址大小:" << sizeof(&a) << " 指针a本身地址:" << &a << std::endl;
+    std::cout << "指针a指向的地址的大小:" << sizeof(a) << " 指针a指向的地址:" << a << std::endl;
+    //std::cout << "指针a指向的地址的值大小:" << sizeof(*a) << " 指针a指向的值:" << *a << std::endl;
+    int a, b;
+    std::cout << "请输入要交换的数字:" << std::endl;
+    std::cin >> a >> b;
+    std::cout << "交换前:a=" << a << "\tb=" << b << std::endl;
+    swap(&a, &b);
+    std::cout << "交换后:a=" << a << "\tb=" << b << std::endl;
+    player("吕布", "战士", 15, 1000, 200, 150);
+    */
     return 0;
 }
