235f3b41e39629b45848c4c339e53bc2179b1690
sheep <3114489507@qq.com>
2020-02-27 21:08:27 +0800

    lost update
    Signed-off-by: sheep <3114489507@qq.com>

--- userSrc/sheep/src/main.cpp
+++ userSrc/sheep/src/main.cpp
@@ -1,71 +1,58 @@
-#include <Windows.h>
-#include <iostream> // 首先导入 螺丝刀所在的工具库
-// int jiafa(int a, int b);
-// double xiaoshujiafa(double a, double b);
-// std::string gongchang(std::string a)
-// {
-//     if (a == "牛")
-//         return "牛肉";
-//     else
-//         return "我是牛肉厂 只加工牛肉";
-// }
-// void player(std::string Name, std::string job, int Level, int Hp, int Mp, std::string Weapon, int Attack, std::string Armor, int Defense, int mony, std::string skill1, std::string skill2, std::string skill3)
-// {
-//     std::cout << "姓名:" << Name << std::endl
-//               << "职业:" << job << std::endl
-//               << "等级:" << Level << std::endl
-//               << "血量:" << Hp << std::endl
-//               << "蓝量" << Mp << std::endl
-//               << "武器:" << Weapon << std::endl
-//               << "攻击力:" << Attack << std::endl
-//               << "防具:" << Armor << std::endl
-//               << "防御力:" << Defense << std::endl
-//               << "金钱数:" << mony << std::endl
-//               << job << "技能一" << skill1 << std::endl
-//               << job << "技能二" << skill2 << std::endl
-//               << job << "技能三" << skill3 << std::endl;
-// }
-// void aaa(std::string *a, std::string *b)
-// {
-//     *a = *a ^ *b;
-//     *b = *a + *b;
-//     *a = *a + *b;
-// }
+#include <ctime>
+#include <iostream>
+const std::string ConstellNamesArray[12][2] = {
+    {"山羊座", "水瓶座"},
+    {"水瓶座", "双鱼座"},
+    {"双鱼座", "白羊座"},
+    {"白羊座", "金牛座"},
+    {"金牛座", "双子座"},
+    {"双子座", "巨蟹座"},
+    {"巨蟹座", "狮子座"},
+    {"狮子座", "处女座"},
+    {"处女座", "天秤座"},
+    {"天秤座", "天蝎座"},
+    {"天蝎座", "射手座"},
+    {"射手座", "山羊座"},
+};
+const int ConstelldatesArray[12] = {20, 19, 21, 20, 21, 22, 23, 23, 23, 24, 23, 22};
+class Player
+{
+  public:
+    Player(std::string DaughterName, std::string FatherName, int BirthDayMonth, int BirthDaydate)
+        : _DaughterName(DaughterName), _FatherName(FatherName), _BirthDayMonth(BirthDayMonth), _BirthDaydate(BirthDaydate)
+    {
+        this->_DaughterConstell = ConstellNamesArray[this->_BirthDayMonth - 1][this->_BirthDaydate / ConstelldatesArray[this->_BirthDayMonth - 1]];
+    }
+    void PrintInformation();
 
-void diyiti(int kuan_du)
-{
-    for (int i = 1; i <= kuan_du; i++)
-    {
-        for (int j = 0; j < i; j++)
-            std::cout << "*";
-        std::cout << std::endl;
-    }
-}
-void dierti(int kuan_du)
-{
-    for (int i = 1; i <= kuan_du; i++)
-    {
-        for (int j = i; j < kuan_du; j++)
-        {
-            std::cout << " ";
-        }
-        for (int k = 1; k <= 2 * i; k++)
-        {
-            if (k % 2 != 0)
-            {
-                std::cout << "*";
-            }
-            else
-            {
-                std::cout << " ";
-            }
-        }
-        std::cout << std::endl;
-    }
-}
+  private:
+    std::string _DaughterName = NULL; //父亲名字
+    std::string _FatherName = NULL;   //女儿名字
+    int _BirthDayMonth = NULL;        //生日月份
+    int _BirthDaydate = NULL;         //生日日期
+    std::string _DaughterConstell = NULL;
+};
 int main()
 {
-    system("cls");
-    dierti(10);
-    return 0;
+    system("chcp 65001");
+    std::string FN;
+    std::string DN;
+    int DM, DD;
+    std::cout << "父亲的姓名:";
+    std::cin >> FN;
+    std::cout << "女儿的姓名:";
+    std::cin >> DN;
+    std::cout << "女儿的生日(MM DD):";
+    std::cin >> DM >> DD;
+    Player *player = nullptr;
+    player = new Player(DN, FN, DM, DD);
+    player->PrintInformation();
+}
+void Player::PrintInformation()
+{
+    std::cout << "女儿的信息:" << std::endl
+              << "姓名:" << this->_DaughterName << std::endl
+              << "生日:1659-" << this->_BirthDayMonth << "-" << this->_BirthDaydate << std::endl
+              << "星座:" << this->_DaughterConstell;
+    system("pause");
 }
