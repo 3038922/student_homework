dffb53fff17efa9041a6b52f237d4d44010e9c0e
fish <2940028639@qq.com>
2020-03-02 09:35:21 +0800

    fish作业已完成,但不知道为什么会报错
    Signed-off-by: fish <2940028639@qq.com>

--- userSrc/fish/src/main.cpp
+++ userSrc/fish/src/main.cpp
@@ -1,21 +1,40 @@
+#include "publicAPI.hpp"
 #include <iostream>
 #include <windows.h>
-int jiaohuan(int *a, int *b)
+
+class fish
 {
-    if (*a < *b)
+
+  public:
+    fish(std::string a, std::string b) : _name(a), _color(b)
     {
-        int lingshi = *a;
-        *b = *a;
-        lingshi = *b;
+        std::cout << "fish类,初始化成功" << std::endl;
     }
-    return 0;
-}
+    ~fish()
+    {
+        std::cout << "fish类,删除成功" << std::endl;
+    }
+    void ShowName()
+    {
+        std::cout << "我是" << _name << std::endl;
+    }
+    void ShowType()
+    {
+        std::cout << _name << "是" << _type << std::endl;
+    }
+
+  private:
+    {
+        std::string _name, tybe;
+    }
+};
 int main()
 {
     system("chcp 65001");
-    while (1 < 100)
-    {
-        std::string *a = new std::string("hh");
-        Sleep(10);
-    }
+    fish w("周宇涵", "黄色");
+    fish g("鱼", "红色");
+    w.ShowName();
+    g.ShowName();
+    w.ShowType();
+    g.ShowType();
 }
