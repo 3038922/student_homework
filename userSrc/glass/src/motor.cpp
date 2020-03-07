3d51bf5139fac2ceb843ff4c7e6d16466ce22b5b
glass <3544505221@qq.com>
2020-03-07 17:38:14 +0800

    glass交了作业
    Signed-off-by: glass <3544505221@qq.com>

--- userSrc/glass/src/motor.cpp
+++ userSrc/glass/src/motor.cpp
@@ -1,4 +1,5 @@
-#include "motor.hpp"
+#include <iostream>
+#include <windows.h>
 // Motor::Motor(int port, int pan, int gear) : _port(port), _pan(pan), _gear(gear)
 // {
 //     std::cout << _port << "号马达初始化成功" << std::endl;
@@ -50,91 +51,121 @@
    * @param gear //0 红齿轮  1 绿齿轮 2 蓝齿轮
    */
 
-Motor::Motor(int port, int zhengfan, int gear) : _port(port), _zhengfan(zhengfan), _gear(gear)
-{
-    std::cout << "马达类添加成功" << std::endl;
-}
-Motor::~Motor()
-{
-    std::cout << "马达类删除成功" << std::endl;
-}
+// Motor::Motor(int port, int zhengfan, int gear) : _port(port), _zhengfan(zhengfan), _gear(gear)
+// {
+//     std::cout << "马达类添加成功" << std::endl;
+// }
+// Motor::~Motor()
+// {
+//     std::cout << "马达类删除成功" << std::endl;
+// }
 
-void Motor::set(int pwm)
-{
-    std::cout << "马达" << _port << "号当前PWM:" << pwm * _zhengfan << std::endl;
-}
+// void Motor::set(int pwm)
+// {
+//     std::cout << "马达" << _port << "号当前PWM:" << pwm * _zhengfan << std::endl;
+// }
 
 /**
      * @brief 设置马达正转还是反转
      * 
      * @param zhengfan 正转填1 反转填-1 
-     */
-void Motor::setZhengfan(int zhengfan)
-{
-    if (gear == 0)
-    {
-        std::cout << "马达" << _port << "号当前齿轮比:红齿轮" << std::endl;
-    }
-    else if (gear == 1)
-    {
-        std::cout << "马达" << _port << "号当前齿轮比:绿齿轮" << std::endl;
-    }
-    else if (gear == 2)
-    {
-        td::cout << "马达" << _port << "号当前齿轮比:蓝齿轮" << std::endl;
-    }
-    else if (gear < 0 || gear > 2)
-    {
-        std::cout << "没有该齿轮" << std::endl;
-    }
-}
-/**
-     * @brief 设置当前马达的齿轮比
-     * 
-     * @param gear 0红齿轮 1 绿齿轮 2 蓝齿轮
-     */
-void Motor::setGear(int gear)
-{
-    if (_gear == 0)
-    {
-        std::cout << "红色" << std::endl;
-    }
-    else if (_gear == 1)
-    {
-        std::cout << "绿色" << std::endl;
-    }
-    else if (_gear == 2)
-    {
-        std::cout << "蓝色" << std::endl;
-    }
-    else if (_gear < 0 || _gear > 2)
-    {
-        std::cout << "没有该齿轮颜色" << std::endl;
-    }
-}
-/**
-     * @brief 获取当前齿轮的颜色
-     * 
-     */
+//      */
+// void Motor::setZhengfan(int zhengfan)
+// {
+//     if (gear == 0)
+//     {
+//         std::cout << "马达" << _port << "号当前齿轮比:红齿轮" << std::endl;
+//     }
+//     else if (gear == 1)
+//     {
+//         std::cout << "马达" << _port << "号当前齿轮比:绿齿轮" << std::endl;
+//     }
+//     else if (gear == 2)
+//     {
+//         td::cout << "马达" << _port << "号当前齿轮比:蓝齿轮" << std::endl;
+//     }
+//     else if (gear < 0 || gear > 2)
+//     {
+//         std::cout << "没有该齿轮" << std::endl;
+//     }
+// }
+// /**
+//      * @brief 设置当前马达的齿轮比
+//      *
+//      * @param gear 0红齿轮 1 绿齿轮 2 蓝齿轮
+//      */
+// void Motor::setGear(int gear)
+// {
+//     if (_gear == 0)
+//     {
+//         std::cout << "红色" << std::endl;
+//     }
+//     else if (_gear == 1)
+//     {
+//         std::cout << "绿色" << std::endl;
+//     }
+//     else if (_gear == 2)
+//     {
+//         std::cout << "蓝色" << std::endl;
+//     }
+//     else if (_gear < 0 || _gear > 2)
+//     {
+//         std::cout << "没有该齿轮颜色" << std::endl;
+//     }
+// }
+// /**
+//      * @brief 获取当前齿轮的颜色
+//      *
+//      */
 
-/*******************以下是获取类成员函数**********************/
-/**
-     * @brief 获取当前的PWM值
-     * 
-     * @return int 返回当前的PWM值 +-127
-     */
-int Motor::getPwm()
+// /*******************以下是获取类成员函数**********************/
+// /**
+//      * @brief 获取当前的PWM值
+//      *
+//      * @return int 返回当前的PWM值 +-127
+//      */
+// int Motor::getPwm()
+// {
+//     std::cout << _pwm + 127 * _zhengfan << std::endl;
+//     return 0;
+// }
+// /**
+//      * @brief 获取当前马达的端口
+//      *
+//      * @return int 返回马达的端口好 1-21
+//      */
+// int Motor::getPort()
+// {
+//     std::cout << _port << std::endl;
+//     return 0;
+// }
+class 眼镜
 {
-    std::cout << _pwm + 127 * _zhengfan << std::endl;
-    return 0;
-}
-/**
-     * @brief 获取当前马达的端口
-     * 
-     * @return int 返回马达的端口好 1-21
-     */
-int Motor::getPort()
+};
 {
-    std::cout << _port << std::endl;
-    return 0;
+  public:
+    眼镜(std::string 镜片) : int _镜片(镜片)
+    {
+        std::cout << "眼镜类创建成功" << std::endl;
+    }
+    void 显示镜片种类()
+    {
+        std::cout << "镜片种类" << _镜片 << std::endl;
+    }
+    std::string_镜框种类;
+    void 选择镜框种类(std::string j)
+    {
+        j = _镜框种类
+    }
+    void 显示镜框种类()
+    {
+        std::cout << "镜框种类是:" << std::endl;
+    }
+};
+int main(int argc, char *argv[])
+{
+    眼镜 a("第一幅");
+    a.显示镜片种类();
+    a.选择镜框种类("圆框");
+    a.显示镜框种类()
 }
